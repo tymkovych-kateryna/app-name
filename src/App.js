7010bcb983b94f705923540a6af54acd0263aa80
Alina220 <ihnatych.alina@student.uzhnu.edu.ua>
2023-12-17 19:46:35 +0200

    smth

--- src/App.js
+++ src/App.js
@@ -8,71 +8,7 @@ function App() {
     <div className="trello-container">
         <Navbar/>
         <BoardContent/>
-      {/* <nav className='app-navbar'>Navigation Bar</nav>
-      <nav className='board-navbar'>Board Bar</nav> */}
-
-      {/* <div className='board-columns'>
-        <div className='columns'>
-          <header>Header</header>
-          <ul>
-            <li>
-              <img src={frog} alt="smth"></img>
-              First task
-            </li>
-            <li>Second task</li>
-            <li>Third task</li>
-          </ul>
-          <footer>Add task</footer>
-        </div>
-
-        <div className='columns'>
-          <header>Header</header>
-          <ul>
-            <li>
-              <img src={frog} alt="smth"></img>
-              First task
-            </li>
-            <li>Second task</li>
-          </ul>
-          <footer>Add task</footer>
-        </div>
-
-        <div className='columns'>
-          <header>Header</header>
-          <ul>
-            <li>
-              <img src={frog} alt="smth"></img>
-              First task
-            </li>
-            <li>Second task</li>
-          </ul>
-          <footer>Add task</footer>
-        </div>
-
-        <div className='columns'>
-          <header>Header</header>
-          <ul>
-            <li>
-              <img src={frog} alt="smth"></img>
-              First task
-            </li>
-            <li>Second task</li>
-          </ul>
-          <footer>Add task</footer>
-        </div>
-
-        <div className='columns'>
-          <header>Header</header>
-          <ul>
-            <li>
-              <img src={frog} alt="smth"></img>
-              First task
-            </li>
-            <li>Second task</li>
-          </ul>
-          <footer>Add task</footer>
-        </div>
-      </div> */}
+     
     </div>
   );
 }
